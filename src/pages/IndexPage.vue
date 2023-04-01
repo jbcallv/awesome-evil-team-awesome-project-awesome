d365069bf6867933865756b11576aa644efe88d5
Joseph Call <jbcallv@gmail.com>
2023-03-31 20:49:39 -0400

    homepage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,26 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="row full-width justify-center items-center">
+      <div class="col-3 text-h1 text-weight-light text">Mix me a...</div>
+
+      <div class="col-3">
+        <div class="custom-input">
+          <input
+            :type="text"
+            :value="value"
+            @input="$emit('input', $event.target.value)"
+          />
+          <div class="underline"></div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
