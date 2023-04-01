232d0a898873ddb3eeb0b3a31cb20363d18e0b43
Joseph Call <jbcallv@gmail.com>
2023-03-31 23:44:02 -0400

    first front-end page ui design

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,30 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="row full-width justify-center items-center">
-      <div class="col-3 text-h1 text-weight-light text">Mix me a...</div>
+    <div class="column q-gutter-md">
+      <div class="row">
+        <div class="col-12 text text-h1 text-weight-light gt-sm">
+          Mixify me a...
+        </div>
 
-      <div class="col-3">
-        <div class="custom-input">
-          <input
-            :type="text"
-            :value="value"
-            @input="$emit('input', $event.target.value)"
+        <div class="col-12 text text-h3 text-weight-light lt-md">
+          Mixify me a...
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+          <q-input label="Song" v-model="songMixify" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">
+          <q-btn
+            rounded
+            size="lg"
+            label="Mixify"
+            style="background-color: #a94d5a"
           />
-          <div class="underline"></div>
         </div>
       </div>
     </div>
@@ -18,9 +32,17 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+
+  setup() {
+    const songMixify = ref("");
+
+    return {
+      songMixify,
+    };
+  },
 });
 </script>
